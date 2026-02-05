c7562c9f2f3744fbaec3690c3c3ad881da8592f0
SHIVAM GARG <148767875+garg05shivam@users.noreply.github.com>
2026-02-05 10:32:11 +0530

    initial backend setup

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-import app from "./app.js"
-const PORT=process.env.PORT || 3000;
+import app from "./app.js";
+import figlet from "figlet";
+import chalk from "chalk";
 
-app.listen(PORT,(err,data)=>{
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    figlet("Server Started", (err, data) => {
+        if (err) {
+            console.log(chalk.red("❌ Figlet error"));
+            return;
+        }
+
+        console.log(chalk.green(data));
+        console.log(
+            chalk.blue.bold(`🚀 Running on: http://localhost:${PORT}`)
+        );
+    });
 });
